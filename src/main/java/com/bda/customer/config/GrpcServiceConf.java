45e4d7d389ac768b2166ba26431e8f8156b68f7f
Carlos Roberto Jeff Montoya <cjeff@bancatlan.hn>
2024-12-03 13:43:05 -0600

    fix: change grcpPorts

--- src/main/java/com/bda/customer/config/GrpcServiceConf.java
+++ src/main/java/com/bda/customer/config/GrpcServiceConf.java
@@ -3,23 +3,26 @@ package com.bda.customer.config;
 import com.bda.customer.service.CustomerGrpcService;
 import io.grpc.Server;
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.InetSocketAddress;
 
-@Configuration
+//@Configuration
 public class GrpcServiceConf {
-    @Bean
-    public Server grpcServer(CustomerGrpcService customerProtoService) throws Exception {
-        Server server = NettyServerBuilder.forAddress(new InetSocketAddress("0.0.0.0", 9091))
-                .addService(customerProtoService)
-                .build();
-        server.start();
-
-
-        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
-
-        return server;
-    }
+//    @Value("${grpc.server.port}")
+//    private int grpcPort;
+//    @Bean
+//    public Server grpcServer(CustomerGrpcService customerProtoService) throws Exception {
+//        Server server = NettyServerBuilder.forAddress(new InetSocketAddress("0.0.0.0", grpcPort))
+//                .addService(customerProtoService)
+//                .build();
+//        server.start();
+//
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+//
+//        return server;
+//    }
 }
